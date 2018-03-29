e69c67ac4657fb2787453bde131403e155b63146
SomeRom71 <somerom71@gmail.com>
2018-03-29 18:39:14 +0300

    pd files

--- src/js/page.js
+++ src/js/page.js
@@ -1,44 +1,19 @@
+import Inputmask from "inputmask";
 $(function() {
     // svg4everybody init
-    svg4everybody();
+    svg4everybody(); 
     
-	var popupOpenBtn = document.querySelector(".popup__open");
-	var popupCloseBtn = document.querySelector(".popup__close"); 
-	var links = document.querySelectorAll(".options__item");
-	var smLinks = document.querySelectorAll(".sm-link");
-	
-	popupOpenBtn.addEventListener("click", popupDisplay);
-	popupCloseBtn.addEventListener("click", popupDisplay);
+    var selector = document.getElementByClassName(".mask");
+	var im = new Inputmask("99-9999999");
+	im.mask(selector);
+	var popupOpenBtn = document.querySelectorAll(".popup__btn");
 
-	smLinks.forEach(function(smlink){
-		smlink.addEventListener("click", smDisplay);
+	popupOpenBtn.forEach(function(popup){
+		popup.addEventListener("click", popupDisplay);
 	});
-
-	links.forEach(function(link){
-		link.addEventListener("click", handleAccordeonClick);
-	});
-
-	function handleAccordeonClick(e) {
-		e.target.classList.toggle("active");
-	};
-
-	function smDisplay(q) {
-		q.target.parentNode.classList.toggle("active_sm");
-	};
 
 	function popupDisplay() {
     	var popup = document.querySelector(".popup");
     	popup.classList.toggle("popup_visible");
 	};
-
-	YMaps.jQuery(function () {
-        var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
-        map.setCenter(new YMaps.GeoPoint(30.491342, 59.932613), 14);
-        var s = new YMaps.Style();
-		s.iconStyle = new YMaps.IconStyle()
-		s.iconStyle.href = "images/map-point.png";
-		s.iconStyle.size = new YMaps.Point(42, 62);
-        var placemark = new YMaps.Placemark(new YMaps.GeoPoint(30.490659, 59.933887), {style: s});
-        map.addOverlay(placemark);
-    })
 });
